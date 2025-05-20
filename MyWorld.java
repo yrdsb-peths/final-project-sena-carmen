bb1f4d5acf7a4f40c25406b3b32b29b9ee06ecb7
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-05-20 14:33:38 -0400

    Created coin class and created the coin object in world class

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Coin coin = new Coin();
+        addObject(coin, 100, 200);
     }
 }
