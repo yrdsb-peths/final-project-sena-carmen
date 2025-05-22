6647e6b8cbdf8beeec502231f0e86963f599f278
CC-iam <1202manman@gmail.com>
2025-05-22 13:55:12 -0400

    Create title screen

---
+++ TitleScreen.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen
+ * 
+ * @author Carmen Cheung 
+ * @version May 12, 2025
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("We will think about it", 60);
+    Label subLabel = new Label("Click <space> to enter game",30); 
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+
+        addObject(titleLabel, getWidth()/2, 150);
+        addObject(subLabel, getWidth()/2, 250);
+        setBackground(new GreenfootImage("background.png"));
+    }
+
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld); 
+        }
+    }
+}
