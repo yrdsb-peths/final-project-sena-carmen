1ef0220c9c57d0b02b4266a1254ed7878590ba77
CC-iam <1202manman@gmail.com>
2025-05-30 14:27:19 -0400

    change score label size

--- MyWorld.java
+++ MyWorld.java
@@ -13,7 +13,7 @@ public class MyWorld extends World {
         super(600, 400, 1, false);
         
         scoreLabel = new Label(0, 80);
-        addObject(scoreLabel, 50, 50);
+        addObject(scoreLabel, 40, 40);
         
         startBall1Timer();  
         startBall2Timer();
